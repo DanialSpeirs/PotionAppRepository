d9d3364693294ab5cadb09d2c50ad7e00c20b1f6
Danial Taylor <danial@graphventure.com>
2016-05-09 13:41:46 -0500

    Added ClickCharts

---
+++ Potions App/ListDetailViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ListDetailViewController.swift
+//  Potions App
+//
+//  Created by Family on 5/6/16.
+//  Copyright © 2016 Danial Taylor. All rights reserved.
+//
+
+import UIKit
+
+class ListDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
